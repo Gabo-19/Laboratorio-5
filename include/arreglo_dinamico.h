e43e31da89fcb0049664471122adcf8538942406
Gabo <gabo1909@Gabo.myguest.virtualbox.org>
2025-06-15 14:29:56 -0600

    agregando crear arrgelo y agregar elemento

--- include/arreglo_dinamico.h
+++ include/arreglo_dinamico.h
@@ -1,1 +1,18 @@
 // header de arrgelo dinamico
+
+
+
+
+#ifndef ARREGLO_DINAMICO_H
+#define ARREGLO_DINAMICO_H
+
+typedef struct {
+    int *datos;
+    int capacidad;
+    int longitud;
+} ArregloDinamico;
+
+ArregloDinamico* crearArreglo(int capacidadInicial);
+void agregarElemento(ArregloDinamico* arr, int valor);
+
+#endif
--- src/arreglo_dinamico.c
+++ src/arreglo_dinamico.c
@@ -1,1 +1,22 @@
 // archivo de arreglo dinamico
+
+
+
+#include <stdlib.h>
+#include "arreglo_dinamico.h"
+
+ArregloDinamico* crearArreglo(int capacidadInicial) {
+    ArregloDinamico* arr = malloc(sizeof(ArregloDinamico));
+    arr->datos = malloc(sizeof(int) * capacidadInicial);
+    arr->capacidad = capacidadInicial;
+    arr->longitud = 0;
+    return arr;
+}
+
+void agregarElemento(ArregloDinamico* arr, int valor) {
+    if (arr->longitud == arr->capacidad) {
+        arr->capacidad *= 2;
+        arr->datos = realloc(arr->datos, arr->capacidad * sizeof(int));
+    }
+    arr->datos[arr->longitud++] = valor;
+}
