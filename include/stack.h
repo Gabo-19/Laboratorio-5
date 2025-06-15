8acb475b486652802205e60f7cac90e1a94c703b
Juanpa MR <juanpamr@Ubuntu.myguest.virtualbox.org>
2025-06-15 15:24:05 -0600

    Push, Pop, Peek

--- include/stack.h
+++ include/stack.h
@@ -1,1 +1,20 @@
 // header de pila
+
+#ifndef STACK_H
+#define STACK_H
+
+typedef struct Nodo {
+    int dato;
+    struct Nodo* siguiente;
+} Nodo;
+
+typedef struct {
+    Nodo* tope;
+} Pila;
+
+Pila* crearPila();
+void push(Pila* pila, int valor);
+int pop(Pila* pila);
+int peek(Pila* pila);
+
+#endif
--- src/stack.c
+++ src/stack.c
@@ -1,1 +1,30 @@
 // fuente
+
+#include <stdlib.h>
+#include "stack.h"
+
+Pila* crearPila() {
+    Pila* pila = malloc(sizeof(Pila));
+    pila->tope = NULL;
+    return pila;
+}
+
+void push(Pila* pila, int valor) {
+    Nodo* nuevo = malloc(sizeof(Nodo));
+    nuevo->dato = valor;
+    nuevo->siguiente = pila->tope;
+    pila->tope = nuevo;
+}
+
+int pop(Pila* pila) {
+    if (!pila->tope) return -1;
+    Nodo* temp = pila->tope;
+    int valor = temp->dato;
+    pila->tope = temp->siguiente;
+    free(temp);
+    return valor;
+}
+
+int peek(Pila* pila) {
+    return pila->tope ? pila->tope->dato : -1;
+}
